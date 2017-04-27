526a63b300614906428eb976b889b03694df2dff
githome <planetding@gmail.com>
2017-04-26 23:50:24 -0400

    Added other feasures to the previous version, including menu (toggle traffic and change map type works), zoom, compass, and home button. also learned to change marker type, and make marker draggable etc.

--- app/src/main/java/com/example/plane/gmap_3_md/gmap3Activity.java
+++ app/src/main/java/com/example/plane/gmap_3_md/gmap3Activity.java
@@ -1,14 +1,32 @@
 package org.planetding.gmap_3_md;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.UiSettings;
+
+import com.google.android.gms.location.LocationListener;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 /** from web
 package com.example.google.maps.demo;
@@ -24,15 +42,31 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import android.widget.Toast;
+
+
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-import org.planetding.gmap_3_md.R;
+
+
+/** added by MD */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/** import org.planetding.gmap_3_md.R; */
 /**
  * @author saxman MainActivity
 
@@ -136,10 +170,15 @@ public class gmap3Activity extends FragmentActivity {
     }
 }
 
-/** */
-public class gmap3Activity extends FragmentActivity implements OnMapReadyCallback {
+/**originally extends: FragmentActivity, changed to AppCompatActivity, and the missing title bar showed up! MD 4-25-17 */
+
+public class gmap3Activity extends AppCompatActivity implements OnMapReadyCallback
+      //  ConnectionCallbacks
+        // , OnConnectionFailedListener
+    {
 
     private GoogleMap mMap;
+    public double mLat, mLng;
 /**
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -169,8 +208,54 @@ public class gmap3Activity extends FragmentActivity implements OnMapReadyCallbac
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+      //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        if (mLastLocation !=null) {
+               mLat = mLastLocation.getLatitude();
+               mLng = mLastLocation.getLongitude();
+        }
+        else {
+            mLat = 20;
+            mLng = 20;
+        }
+
+     //   LatLng currentLL = new LatLng(String.valueOf(mLastLocation.getLatitude(),String.valueOf(mLastLocation.getLatitude());
+        LatLng currentLL = new LatLng(mLat,mLng);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLL));
+
+        //added by MD to add the center map button to my current location, and add a blue dot to my current location
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng( ));
     }
+
+
+/**
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        // LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+ if (mMap != null) {
+ int permission = ContextCompat.checkSelfPermission(this,
+ Manifest.permission.ACCESS_FINE_LOCATION);
+
+ if (permission == PackageManager.PERMISSION_GRANTED) {
+ mMap.setMyLocationEnabled(true);
+ } else {
+ requestPermission(
+ Manifest.permission.ACCESS_FINE_LOCATION,
+ LOCATION_REQUEST_CODE);
+ }
+ }
+    */
+
 
     private static final String LOG_TAG = "ExampleApp";
 
@@ -178,12 +263,123 @@ public class gmap3Activity extends FragmentActivity implements OnMapReadyCallbac
 
     protected GoogleMap map;
 
+    /**
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gmap3);
         setUpMapIfNeeded();
     }
+*/
+
+//copied from another file oncreate, with the MapFragment, with this, the starting map is then oriented to the
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+        protected LocationRequest mLocationRequest;
+
+        private final int[] MAP_TYPES = { GoogleMap.MAP_TYPE_SATELLITE,
+            GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_NONE };
+        private int curMapTypeIndex = 0;
+
+        protected void createLocationRequest() {
+            mLocationRequest = new LocationRequest();
+
+            // Sets the desired interval for active location updates. This interval is
+            // inexact. You may not receive updates at all if no location sources are available, or
+            // you may receive them slower than requested. You may also receive updates faster than
+            // requested if other applications are requesting location at a faster interval.
+            mLocationRequest.setInterval(30);
+
+            // Sets the fastest rate for active location updates. This interval is exact, and your
+            // application will never receive updates faster than this value.
+            mLocationRequest.setFastestInterval(10);
+
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmap3);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // To connect to the API, you need to create an instance of the Google Play services API client.
+        // For details about using the client, see the guide to Accessing Google APIs.
+        // In your activity's onCreate() method, create an instance of Google API Client,
+        // using the GoogleApiClient.Builder class to add the LocationServices API, as the following code snippet shows.
+        // Create an instance of GoogleAPIClient.
+        /**       if (mGoogleApiClient == null) {
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+         .addConnectionCallbacks(this)
+         //         .addOnConnectionFailedListener(this)
+         .addApi(LocationServices.API)
+         .build();
+         }
+
+         LocationRequest locationRequest  = LocationRequest.create()
+         .setNumUpdates(1)
+         .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+         .setInterval(0);
+
+         //       LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,locationRequest, this);
+
+         //       LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+
+         **/
+    }
+    /**
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+     */
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+      /**  if (mLastLocation != null) {
+            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
+                    mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
+                    mLastLocation.getLongitude()));
+        } else {
+            // Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(gmap3Activity.this,
+                "onConnectionSuspended: " + String.valueOf(i),
+                Toast.LENGTH_LONG).show();
+    }
+     **/
 
     @Override
     protected void onResume() {
@@ -193,8 +389,7 @@ public class gmap3Activity extends FragmentActivity implements OnMapReadyCallbac
 
     private void setUpMapIfNeeded() {
         if (map == null) {
-            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
+            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             if (map != null) {
                 setUpMap();
             }
@@ -202,8 +397,20 @@ public class gmap3Activity extends FragmentActivity implements OnMapReadyCallbac
     }
 
     private void setUpMap() {
+        // added by MD to set up map configuration
+       UiSettings UiSettings = map.getUiSettings();
+      //  UiSettings.setZoomControlsEnabled(false);
+      //  UiSettings.setMyLocationButtonEnabled( false );
+      //  UiSettings.setScrollGesturesEnabled(false);
+       // UiSettings.setZoomGesturesEnabled(false);
+       // UiSettings.setTiltGesturesEnabled(false);
+       // UiSettings.setRotateGesturesEnabled(false);
+
         // Retrieve the city data from the web service
         // In a worker thread since it's a network operation.
+
+
+
         new Thread(new Runnable() {
             public void run() {
                 try {
@@ -260,8 +467,12 @@ public class gmap3Activity extends FragmentActivity implements OnMapReadyCallbac
             // Create a marker for each city in the JSON data.
             JSONObject jsonObj = jsonArray.getJSONObject(i);
             map.addMarker(new MarkerOptions()
-                    .title(jsonObj.getString("name"))
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.en))
+                    .title("<a href='" + jsonObj.getString("name" )+ "'>" + "video" + "</a>" )
                     .snippet(Integer.toString(jsonObj.getInt("population")))
+                    .draggable(true)
+        //            .snippet(jsonObj.getString("myurl"))
                     .position(new LatLng(
                             jsonObj.getJSONArray("latlng").getDouble(0),
                             jsonObj.getJSONArray("latlng").getDouble(1)
@@ -269,6 +480,72 @@ public class gmap3Activity extends FragmentActivity implements OnMapReadyCallbac
             );
         }
     }
+
+        /** these are about menu added
+         * **/
+
+        private void toggleTraffic() {
+            map.setTrafficEnabled( !map.isTrafficEnabled() );
+        }
+
+        private void cycleMapType() {
+            if( curMapTypeIndex < MAP_TYPES.length - 1 ) {
+                curMapTypeIndex++;
+            } else {
+                curMapTypeIndex = 0;
+            }
+
+            map.setMapType( MAP_TYPES[curMapTypeIndex] );
+        }
+
+        /** To instruct Android to use your options menu, open the Activity class you want it to appear with.
+     * Add the following method to your Java code, inside the class declaration and after the "onCreate" method
+       **/
+        public boolean onCreateOptionsMenu(Menu menu) {
+
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+       /** To respond to user interaction with your menu, you first need to detect it within your Activity class.
+        * Add the following method outline after the "onCreateOptionsMenu" method.
+        * Inside this method, which returns a boolean value, you can add code to respond to each particular item.
+        * The system will automatically call the "onOptionsItemSelected" method when the user chooses any of the options menu items.
+        **/
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_clear: {
+      //              getMap().clear();
+                    return true;
+                }
+                case R.id.action_circle: {
+      //              drawCircle( new LatLng( mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude() ) );
+                    return true;
+                }
+                case R.id.action_polygon: {
+      //              drawPolygon( new LatLng( mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude() ) );
+                    return true;
+                }
+                case R.id.action_overlay: {
+      //              drawOverlay( new LatLng( mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude() ), 250, 250 );
+                    return true;
+                }
+                case R.id.action_traffic: {
+                    toggleTraffic();
+                    return true;
+                }
+                case R.id.action_cycle_map_type: {
+                    cycleMapType();
+                    return true;
+                }
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+
+            }
+
 
 
 }
